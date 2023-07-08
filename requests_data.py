9fa7914f4f6704e65ed4b674f43cbe172ce59c27
Denys Dudnik <dudnikdenys777@gmail.com>
2023-07-08 15:36:36 +0300

    tests for test task

---
+++ requests_data.py
@@ -0,0 +1,13 @@
+from secret_data import KEY, TOKEN
+
+BASE_URL = 'https://api.trello.com/1/'
+
+BASE_DATA = {
+    'key': KEY,
+    'token': TOKEN
+}
+
+BASE_HEADERS = {
+    'Accept': 'application/json'
+}
+
